05ac8e901077b1527d9fea49036341c8ca468b06
Pradip P Caulagi <caulagi@gmail.com>
2014-04-22 09:44:18 +0530

    First stab at rna transcription

---
+++ clojure/rna-transcription/dna.clj
@@ -0,0 +1,41 @@
+;;;; Rna Transcription
+;;;;
+;;;; Write a program that, given a DNA strand, returns its RNA
+;;;; complement (per RNA transcription).
+;;;;
+;;;; Both DNA and RNA strands are a sequence of nucleotides.
+;;;; The four nucleotides found in DNA are adenine (**A**),
+;;;; cytosine (**C**), guanine (**G**) and thymidine (**T**).
+;;;;
+;;;; The four nucleotides found in RNA are adenine (**A**),
+;;;; cytosine (**C**), guanine (**G**) and uracil (**U**).
+;;;; 
+;;;; Given a DNA strand, its transcribed RNA strand is formed
+;;;; by replacing each nucleotide with its complement:
+;;;;
+;;;; * `G` -> `C`
+;;;; * `C` -> `G`
+;;;; * `T` -> `A`
+;;;; * `A` -> `U`
+
+(ns dna)
+
+(defn- transcribe [strand]
+  "Transcribe this single RNA strand"
+  (case strand
+    \G \C
+    \C \G
+    \T \A
+    \A \U
+    (assert false)))
+
+(defn- to-rna-iterative [str seq]
+  "An iterative process as in SICP to iterate RNA"
+  (if (empty? str)
+    (clojure.string/join "" seq)
+    (to-rna-iterative
+      (rest str)
+      (cons (transcribe (first str)) seq))))
+
+(defn to-rna [str]
+  (to-rna-iterative (clojure.string/reverse str) '()))
