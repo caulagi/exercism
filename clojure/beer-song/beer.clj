f1125f35733d82a4888fb0da88a388446e132c66
Pradip P Caulagi <caulagi@gmail.com>
2014-04-27 20:54:25 +0530

    Deobfuscate code - no arguments corresponds to one beer bottle and we need to pass argument explicitly otherwise.  Also remove over-engineering

--- clojure/beer-song/beer.clj
+++ clojure/beer-song/beer.clj
@@ -8,15 +8,19 @@
 (ns beer
   (:use [clojure.string :only [join]]))
 
-(defn- pluralize [word n]
+(def verse-with-placeholder
+  "%1$s of beer on the wall, %1$s of beer.\nTake one down and pass it around, %2$s of beer on the wall.\n")
+
+(defn- pluralize [n]
+  "Get the number of bottles, properly pluralized"
   (cond
-    (= n 1) (format "%s %s" n word)
-    (= n 0) (str "no more " word "s")
-    :else (format "%s %ss" n word)))
+    (= n 1) (format "%s bottle" n)
+    (= n 0) (str "no more bottles")
+    :else (format "%s bottles" n)))
 
 (defn- repeat-verse
-  ([] "%1$s of beer on the wall, %1$s of beer.\nTake one down and pass it around, %2$s of beer on the wall.\n")
-  ([one] "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"))
+  ([n] (format verse-with-placeholder (pluralize n) (pluralize (dec n))))
+  ([] "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"))
 
 (defn- restart-verse
   []
@@ -25,8 +29,8 @@
 (defn verse [n]
   (cond
     (= n 0) (restart-verse)
-    (= n 1) (repeat-verse 1)
-    :else (format (repeat-verse) (pluralize "bottle" n) (pluralize "bottle" (dec n)))))
+    (= n 1) (repeat-verse)
+    :else (repeat-verse n)))
 
 (defn sing 
   ([start] (sing start 0))
