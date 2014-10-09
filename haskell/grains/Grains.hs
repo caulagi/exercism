c0650008bd919dfb275484de31f1450fe0390e17
Pradip P Caulagi <caulagi@gmail.com>
2014-10-09 14:44:28 +0530

    * x ^ y is integer exponentiation * Use the formula for sum of a geometric series when determining total.

--- haskell/grains/Grains.hs
+++ haskell/grains/Grains.hs
@@ -1,17 +1,7 @@
 module Grains (square, total) where
 
-totalFor :: Integer -> Integer
-totalFor n = sum (map square [1..n])
-
 total :: Integer
-total = totalFor 64
-
-powerOf' :: Integer -> Integer -> Integer -> Integer
-powerOf' _ 1 acc = acc
-powerOf' x n acc = powerOf' x (n - 1) (x * acc)
-
-powerOf :: Integer -> Integer -> Integer
-powerOf x n = powerOf' x n 1
+total = (2 ^ 64) - 1
 
 square :: Integer -> Integer
-square n = powerOf 2 n
+square n = 2 ^ (n-1)
