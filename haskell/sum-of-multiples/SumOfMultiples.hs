e298041d2e0a1fa4874de0846e0f00ee057f9cb5
Pradip P Caulagi <caulagi@gmail.com>
2014-10-12 14:09:29 +0530

    Use any instead of filter

--- haskell/sum-of-multiples/SumOfMultiples.hs
+++ haskell/sum-of-multiples/SumOfMultiples.hs
@@ -3,13 +3,8 @@ module SumOfMultiples (
 , sumOfMultiplesDefault
 ) where
 
--- whether n is a multiple of any number in a
-multipleOf :: [Int] -> Int -> Bool
-multipleOf [] n = False
-multipleOf (x:xs) n = n `mod` x == 0 || multipleOf xs n
-
 sumOfMultiples :: [Int] -> Int -> Int
-sumOfMultiples a n = sum (filter (multipleOf a) [1..n-1])
+sumOfMultiples xs n = sum (filter (\a -> any (\x -> a `mod` x == 0) xs) [1..n-1])
 
 sumOfMultiplesDefault :: Int -> Int
 sumOfMultiplesDefault = sumOfMultiples [3,5]
