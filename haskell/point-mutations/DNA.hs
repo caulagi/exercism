5a82c865805fd20fa95d6edf1e040af0d14fa60d
Pradip P Caulagi <caulagi@gmail.com>
2014-10-13 15:01:32 +0530

    Use zipWith instead of creating tuples

--- haskell/point-mutations/DNA.hs
+++ haskell/point-mutations/DNA.hs
@@ -1,4 +1,4 @@
 module DNA (hammingDistance) where
 
 hammingDistance :: String -> String -> Int
-hammingDistance dna1 dna2 = sum [1 | (x, y) <- zip dna1 dna2, x /= y]
+hammingDistance dna1 dna2 = sum $ zipWith (\x y -> if x /= y then 1 else 0) dna1 dna2
